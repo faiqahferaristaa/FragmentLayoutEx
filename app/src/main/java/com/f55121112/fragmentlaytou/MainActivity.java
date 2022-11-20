488f031c1165fb8c16936731e9fc809c1716a79d
faiqahferaristaa <faiqahferaristah@gmail.com>
2022-11-21 00:43:01 +0800

    Initial commit

---
+++ app/src/main/java/com/f55121112/fragmentlaytou/MainActivity.java
@@ -0,0 +1,33 @@
+package com.f55121112.fragmentlaytou;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private AppBarLayout appBarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appBar_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.AddFragment(new FragmentHome(),"Home");
+        adapter.AddFragment(new FragmentExplore(),"Explore");
+        adapter.AddFragment(new FragmentAccount(),"Account");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
